c35f159f296718bc44bc446f52111a025a3af192
evanjkish <evanjkish@icloud.com>
2021-10-28 22:36:57 -0600

    commit2

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ function App() {
   return (
     <div className="App">
       <UserForm />
-
-      <h3>Your Form Data</h3>
-      <p>{UserForm.firstName}</p>
-      <p>{UserForm.lastName}</p>
-      <p>{UserForm.email}</p>
-      <p>{UserForm.password}</p>
-      <p>{UserForm.confirmPassword}</p>
     </div>
   );
 }
